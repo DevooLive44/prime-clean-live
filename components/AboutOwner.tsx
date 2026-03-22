3517fe3fca85d24424c721172ceff670b1a378ba
DevooLive44 <devran.joo@outlook.de>
2026-03-22 13:06:31 +0100

    Update AboutOwner.tsx

--- components/AboutOwner.tsx
+++ components/AboutOwner.tsx
@@ -40,10 +40,9 @@ const AboutOwner: React.FC = () => {
           <div className="relative order-2 lg:order-1 flex flex-col items-center">
             <div className="relative w-full max-w-md rounded-[2.5rem] overflow-hidden shadow-[0_30px_60px_rgba(0,0,0,0.6)] border border-white/10 bg-slate-900/50 backdrop-blur-sm aspect-[3/4] group">
               {imageSrc ? (
-                <img 
-                  <img
+                <img
   src="/images/dennis.jpg"
-  alt="Devran Dennis Joó"
+  alt="Devran Dennis Joo"
   className="w-full h-full object-cover"
 />
                 />
